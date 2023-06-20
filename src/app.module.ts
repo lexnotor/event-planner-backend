44dc5bd049c4c1bb246217af450e08ae318a3cf3
alexandre-notor <amanichambu48@gmail.com>
2023-06-20 12:43:52 +0200

    feat: generate user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
