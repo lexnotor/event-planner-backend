543696651ff306b68668e2735ee354fdcba83d70
alexandre-notor <amanichambu48@gmail.com>
2023-06-20 12:49:45 +0200

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './modules/user/user.module';
-import { PostModule } from './modules/post/post.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PostModule } from "./modules/post/post.module";
+import { UserModule } from "./modules/user/user.module";
 
 @Module({
-  imports: [UserModule, PostModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UserModule, PostModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
