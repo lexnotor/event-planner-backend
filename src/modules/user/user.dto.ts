4132ff669a304bae7b3dac6fc8a6bfeadcd7f422
alexandre-notor <amanichambu48@gmail.com>
2023-06-23 11:52:22 +0200

    feat: add update user dto

--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -45,4 +45,36 @@ class CreateUserDto {
     types: string = UserTypes.USER;
 }
 
-export { CreateUserDto, UserTypes };
+class UpdateUserDto {
+    @IsOptional()
+    @IsString()
+    @Length(3, 50)
+    firstname: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(3, 50)
+    lastname: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(3, 30)
+    username: string;
+
+    @IsOptional()
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(0, 200)
+    description: string;
+
+    @IsOptional()
+    @IsString()
+    @IsEnum(UserTypes)
+    types: UserTypes;
+}
+
+export { CreateUserDto, UpdateUserDto, UserTypes };
