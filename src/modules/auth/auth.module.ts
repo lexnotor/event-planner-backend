d0da5df700eb04df6746f56b1c5b67c5e987841c
alexandre-notor <amanichambu48@gmail.com>
2023-06-28 05:07:48 +0200

    feat: add auth guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -14,5 +14,6 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
     ],
     controllers: [AuthController],
     providers: [AuthService, JwtService, ConfigService],
+    exports: [JwtService],
 })
 export class AuthModule {}
