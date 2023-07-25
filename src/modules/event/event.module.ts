762f12f295bef4aff6c03f0dcd471ea5939b4ea4
alexandre-notor <amanichambu48@gmail.com>
2023-07-25 14:06:46 +0200

    fix: import jwtModule & AuthModule

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -1,14 +1,18 @@
 import { Module } from "@nestjs/common";
-import { EventController } from "./event.controller";
-import { EventService } from "./event.service";
+import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { EventEntity, EventPhotoEntity } from "./event.entity";
+import { AuthModule } from "../auth/auth.module";
 import { PhotoModule } from "../photo/photo.module";
 import { UserModule } from "../user/user.module";
+import { EventController } from "./event.controller";
+import { EventEntity, EventPhotoEntity } from "./event.entity";
+import { EventService } from "./event.service";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([EventEntity, EventPhotoEntity]),
+        JwtModule,
+        AuthModule,
         PhotoModule,
         UserModule,
     ],
