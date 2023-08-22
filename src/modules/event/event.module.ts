c7914d5adf998d1a04ddbff84f3d9c2c6f80b6bb
alexandre-notor <amanichambu48@gmail.com>
2023-08-22 14:46:59 +0200

    fix: missing dependancy

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -2,19 +2,25 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModule } from "../auth/auth.module";
+import { GigModule } from "../gig/gig.module";
 import { PhotoModule } from "../photo/photo.module";
 import { UserModule } from "../user/user.module";
 import { EventController } from "./event.controller";
-import { EventEntity, EventPhotoEntity } from "./event.entity";
+import { EventEntity, EventGigEntity, EventPhotoEntity } from "./event.entity";
 import { EventService } from "./event.service";
 import { EventGigService } from "./event_gig.service";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([EventEntity, EventPhotoEntity]),
+        TypeOrmModule.forFeature([
+            EventEntity,
+            EventPhotoEntity,
+            EventGigEntity,
+        ]),
         JwtModule,
         AuthModule,
         PhotoModule,
+        GigModule,
         UserModule,
     ],
     controllers: [EventController],
